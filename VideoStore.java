f8633ef655a37b872a8917be5dfadb5c75fdc758
AllenJoshua <151940035+AllenJosh2304@users.noreply.github.com>
2025-06-14 09:33:26 +0530

    Update VideoStore.java

--- VideoStore.java
+++ VideoStore.java
@@ -1,63 +1,63 @@
 import java.util.ArrayList;
 
-public class VideoStore
-{
+public class VideoStore {
     ArrayList<Video> store = new ArrayList<>();
     int videos = 0;
 
-    void addVideo(String name)
-    {
+    void addVideo(String name) {
         store.add(new Video(name));
         System.out.println("Video \"" + name + "\" added successfully");
         videos++;
     }
-    void doCheckout(String name)
-    {
-        for(int i  =0;i<store.size();i++){
+
+    void doCheckout(String name) {
+        for (int i = 0; i < store.size(); i++) {
             Video v = store.get(i);
             String s = v.getName();
-            if (s.equalsIgnoreCase(name))
-            {
+            if (s.equalsIgnoreCase(name)) {
                 v.doCheckout();
+                System.out.println("Video \"" + name + "\" checked out successfully.");
                 return;
             }
         }
-        System.out.println("Sorry Video Not Found");
+        System.out.println("Sorry, video not found.");
     }
-    void doReturn(String name)
-    {
-        for(int i  =0;i<store.size();i++){
+
+    void doReturn(String name) {
+        for (int i = 0; i < store.size(); i++) {
             Video v = store.get(i);
             String s = v.getName();
-            if (s.equalsIgnoreCase(name))
-            {
+            if (s.equalsIgnoreCase(name)) {
                 v.doReturn();
+                System.out.println("Video \"" + name + "\" returned successfully.");
                 return;
             }
         }
-        System.out.println("Sorry Video Not Found");
+        System.out.println("Sorry, video not found.");
     }
-    void receiveRating(String name, int rating)
-    {
-        for(int i  =0;i<store.size();i++){
-            Video v = store.get(i);
-            String s = v.getName();
-            if (s.equalsIgnoreCase(name))
-            {
+
+    void receiveRating(int vidNo, int rating) {
+        try {
+            if (vidNo <= store.size() && vidNo >= 1) {
+                Video v = store.get(vidNo - 1);
                 v.receiveRating(rating);
-                return;
+                System.out.println("Rating received successfully.");
+            } else {
+                System.out.println("Sorry, invalid video number.");
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Sorry, invalid video number.");
         }
-        System.out.println("Sorry Video Not Found");
     }
+
     void listInventory() {
-        if(videos == 0){
-            System.out.println("Sorry there are no videos in the inventory currently ");
+        if (videos == 0) {
+            System.out.println("Sorry, there are no videos in the inventory currently.");
             return;
         }
 
         System.out.println("--------------------------------------------------------");
-        System.out.println("Video Name          Checkout Status       Rating");
+        System.out.printf("%-20s %-20s %-10s\n", "Video Name", "Checkout Status", "Rating");
         for (int i = 0; i < store.size(); i++) {
             Video v = store.get(i);
             System.out.printf("%-20s %-20s %-10s\n", v.getName(), v.getCheckout(), v.getRating());
